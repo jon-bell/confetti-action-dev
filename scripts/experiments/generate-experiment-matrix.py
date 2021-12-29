b82e1fc07afc13159d0bbb8f51847f5baf73876a
Jonathan Bell <jon@jonbell.net>
2021-12-29 17:57:04 -0500

    new experiment...

--- scripts/experiments/generate-experiment-matrix.py
+++ scripts/experiments/generate-experiment-matrix.py
@@ -2,25 +2,28 @@ import sys
 import json
 
 bms = [
-{"id":"ant", "class":"edu.berkeley.cs.jqf.examples.ant.ProjectBuilderTest", "method" : "testWithGenerator",
-"coveragePackages" : "org/apache/tools/ant/*"},
-{"id":"bcel", "class":"edu.berkeley.cs.jqf.examples.bcel.ParserTest", "method" : "testWithGenerator",
-"coveragePackages" : "org/apache/bcel/*"},
+# {"id":"ant", "class":"edu.berkeley.cs.jqf.examples.ant.ProjectBuilderTest", "method" : "testWithGenerator",
+# "coveragePackages" : "org/apache/tools/ant/*"},
+# {"id":"bcel", "class":"edu.berkeley.cs.jqf.examples.bcel.ParserTest", "method" : "testWithGenerator",
+# "coveragePackages" : "org/apache/bcel/*"},
 {"id":"closure", "class" : "edu.berkeley.cs.jqf.examples.closure.CompilerTest", "method": "testWithGenerator",
 "coveragePackages" : "com/google/javascript/jscomp/*"},
-{"id":"maven", "class" : "edu.berkeley.cs.jqf.examples.maven.ModelReaderTest", "method" : "testWithGenerator",
-"coveragePackages" : "org/apache/maven/model/*"},
-{"id":"rhino", "class" : "edu.berkeley.cs.jqf.examples.rhino.CompilerTest", "method" : "testWithGenerator",
-"coveragePackages" : "org/mozilla/javascript/optimizer/*:org/mozilla/javascript/CodeGenerator*"}
+# {"id":"maven", "class" : "edu.berkeley.cs.jqf.examples.maven.ModelReaderTest", "method" : "testWithGenerator",
+# "coveragePackages" : "org/apache/maven/model/*"},
+# {"id":"rhino", "class" : "edu.berkeley.cs.jqf.examples.rhino.CompilerTest", "method" : "testWithGenerator",
+# "coveragePackages" : "org/mozilla/javascript/optimizer/*:org/mozilla/javascript/CodeGenerator*"}
 ]
 
-if(len(sys.argv) == 2):
-    configs = []
-    for x in range(int(sys.argv[1])):
-        for bm in bms:
-            tmp = bm.copy()
-            tmp['runNumber'] = x
-            configs.append(tmp)
-    print(json.dumps({'config':configs}))
-else:
-    print(json.dumps({'config': bms}))
+memorySettings = [
+{"JQF": "6g", "Central":"6g", "Knarr": "6g"},
+{"JQF": "3g", "Central":"3g", "Knarr": "3g"},
+{"JQF": "4g", "Central":"4g", "Knarr": "4g"},
+{"JQF": "4g", "Central":"5g", "Knarr": "3g"},
+]
+configs=[]
+for x in memorySettings:
+    for bm in bms:
+        tmp = bm.copy()
+        tmp['Xmx'] = x
+        configs.append(tmp)
+print(json.dumps({'config':configs}))
